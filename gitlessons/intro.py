c0cc4f5676a9c0306c1e0196c735070df951d777
OmuraCode <akyl.net@gmail.com>
2023-04-11 20:47:43 +0600

    added git instructions

--- gitlessons/intro.py
+++ gitlessons/intro.py
@@ -22,7 +22,12 @@
 # 6. git status - команда для проверки статуса изменились ли?????
 
 
-
+# _____________________
+# 1) git init
+#     2) git add
+#     3) git commit -m ''
+# 4) git remote add origin <url>
+#     5)git push origin main
 
 
 
